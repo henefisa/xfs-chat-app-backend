468de88b88cf15ccced9235a8bc5e4fd94b3f22a
Henefisa <jickyetran@outlook.com>
2022-09-25 00:00:26 +0700

    UPDATE: create user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,21 +1,26 @@
-import { Response } from "express";
-import { CreateUserDto } from "src/dto/user/create-user.dto";
+import { NextFunction, Response } from "express";
+import { CreateUserDto } from "src/dto/user";
 import { User } from "src/entities/user.entity";
 import { RequestWithBody } from "src/shares";
-import dataSource from "src/configs/data-source";
 import { StatusCodes } from "http-status-codes";
 import { hash, genSalt } from "bcrypt";
+import dataSource from "src/configs/data-source";
 
 const userRepository = dataSource.getRepository(User);
 
 export const createUser = async (
   req: RequestWithBody<CreateUserDto>,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
-  const user = new User();
-  user.username = req.body.username;
-  user.password = await hash(req.body.password, await genSalt());
-  const saved = await userRepository.save(user);
+  try {
+    const user = new User();
+    user.username = req.body.username;
+    user.password = await hash(req.body.password, await genSalt());
+    const saved = await userRepository.save(user);
 
-  return res.status(StatusCodes.CREATED).json(saved);
+    return res.status(StatusCodes.CREATED).json(saved);
+  } catch (error) {
+    next(error);
+  }
 };
