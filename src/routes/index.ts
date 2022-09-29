d963afe7d1ed2d9fde34f731f6408a7e98fe7167
khang2038 <trankhang9c2003@gmail.com>
2022-09-29 16:51:51 +0700

    FEAT: CRUD Message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
+import { messageRoutes } from './message.route';
 import { Router } from "express";
 import { UserRoutes } from "./user.route";
 
 const router: Router = Router();
 
 router.use("/users", UserRoutes);
+router.use("/messages",messageRoutes)
 
 export const MainRoutes = router;
---
+++ src/routes/message.route.ts
@@ -0,0 +1,14 @@
+import {
+  createMessage,
+  deleteMessage,
+  getMessage,
+} from 'src/controllers/message.controller';
+import { Router } from 'express';
+
+const router: Router = Router();
+
+router.post('/', createMessage);
+router.get('/:conversationid', getMessage);
+router.delete('/:id', deleteMessage);
+
+export const messageRoutes = router;
