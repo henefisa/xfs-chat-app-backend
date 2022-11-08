b133b79dfbe65a8f81a430017ce5b6b09f8967c2
Henefisa <jickyetran@outlook.com>
2022-11-08 23:01:28 +0700

    fix: test workflows

--- src/configs/Redis.ts
+++ src/configs/Redis.ts
@@ -1,6 +1,12 @@
 import Redis from 'ioredis';
+import { config } from 'dotenv';
 
-const redis = new Redis(6379);
+config();
+
+const redis = new Redis({
+  port: Number(process.env.REDIS_PORT) || 6379,
+  host: process.env.REDIS_HOST || 'localhost',
+});
 
 redis.on('ready', () => {
   console.log('Redis connected');
