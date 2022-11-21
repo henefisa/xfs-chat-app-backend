5052aa3d5dfd5e105082eadcd1e5e5d757f7d92d
khang2038 <84883516+khang2038@users.noreply.github.com>
2022-11-21 08:57:24 +0700

    Feature/get conversation (#69)
    * feat: config conversation
    
    * feat: get conversation

--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -1,3 +1,6 @@
+import { GetConversationOptions } from 'src/interfaces/conversation.interface';
+import { getLimitAndOffset } from 'src/shares/get-limit-and-offset';
+import { GetConversationDto } from 'src/dto/conversation/get-conversation.dto';
 import Database from 'src/configs/Database';
 import { CreateConversationDto } from 'src/dto/conversation/create-conversation.dto';
 import { Conversation } from 'src/entities/conversation.entity';
@@ -17,4 +20,44 @@ export const createConversation = async (dto: CreateConversationDto) => {
 
 export const getOne = async (options: FindOneOptions<Conversation>) => {
   return conversationRepository.findOne(options);
+};
+
+export const getConversationsOfUser = async (
+  userId: string,
+  dto?: GetConversationDto,
+  options?: GetConversationOptions
+) => {
+  const { limit, offset } = getLimitAndOffset({
+    limit: dto?.limit,
+    offset: dto?.offset,
+  });
+
+  const query = conversationRepository.createQueryBuilder('conversation');
+
+  if (!options?.unlimited) {
+    query.skip(offset).take(limit);
+  }
+
+  query
+    .leftJoin('conversation.participants', 'participants')
+    .leftJoin('participants.user', 'users')
+    .andWhere('participants.userId = :userId', { userId });
+
+  if (dto?.q) {
+    query.andWhere(
+      'title ILIKE :q  OR username ILIKE :q OR full_name ILIKE :q',
+      { q: `%${dto.q}%` }
+    );
+  }
+
+  if (options?.id) {
+    query.andWhere('conversation.id = :id', { id: options.id });
+  }
+
+  const [conversations, count] = await query.getManyAndCount();
+
+  return {
+    conversations,
+    count,
+  };
 };
