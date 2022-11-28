99b69ce63922a87573b0d1cb42a29925f7b7c5cb
khang2038 <trankhang9c2003@gmail.com>
2022-11-28 20:38:04 +0700

    refactor: use sub query

--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -43,9 +43,15 @@ export const getConversationsOfUser = async (
     .leftJoinAndSelect('conversation.participants', 'participants')
     .leftJoinAndSelect('participants.user', 'users')
     .where(
-      'conversation.id IN (SELECT "conversationId" FROM participants WHERE "userId" = :userId)',
-      { userId }
-    );
+      'conversation.id IN' +
+        query
+          .subQuery()
+          .select('p.conversationId')
+          .from(Participants, 'p')
+          .where('p.userId = :userId')
+          .getQuery()
+    )
+    .setParameter('userId', userId);
 
   if (dto?.q) {
     query.andWhere(
