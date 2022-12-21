11d2d331214b8de3450148a38d1ccf4f920aa222
khang2038 <trankhang9c2003@gmail.com>
2022-12-21 13:44:27 +0700

    refactor: senlink function

--- src/services/reset-password.service.ts
+++ src/services/reset-password.service.ts
@@ -5,26 +5,22 @@ import { transporter } from './send-otp.service';
 import * as userService from 'src/services/user.service';
 import { NotFoundException } from 'src/exceptions';
 
-export const sendLink = async (email: string, host: string) => {
-  const user = await userService.getOne({ where: { email } });
-
-  if (!user) {
-    throw new NotFoundException('user');
-  }
-
-  const code = otpGenerator.generate(10, {
-    lowerCaseAlphabets: false,
-    upperCaseAlphabets: false,
-    specialChars: false,
-  });
-
-  const mainOptions = {
+const getOptions = (email: string, host: string, id: string, code: string) => {
+  return {
     from: 'RVK Chat App',
     to: email,
     subject: 'RVK Chat app reset password',
     text: ' Hello welcome RVK Chat App, your link reset password ',
-    html: `<p> Hello welcome RVK Chat App, your link reset password <b> ${host}/reset-password/${user.id}?code=${code}</b> </p> `,
+    html: `<p> Hello welcome RVK Chat App, your link reset password <b> ${host}/reset-password/${id}?code=${code}</b> </p> `,
   };
+};
+
+export const sendLink = async (email: string, host: string) => {
+  const user = await userService.getOneOrThrow({ where: { email } });
+
+  const code = otpGenerator.generate(20, { specialChars: false });
+
+  const mainOptions = getOptions(email, host, user.id, code);
 
   transporter.sendMail(mainOptions);
 
