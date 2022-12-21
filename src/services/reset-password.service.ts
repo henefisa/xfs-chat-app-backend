3934e95876677bd06d156869000ce04ec74f9efa
khang2038 <84883516+khang2038@users.noreply.github.com>
2022-12-21 22:40:37 +0700

    feat: forgot password (#144)
    * feat: forgot password
    
    * refactor: senlink function

---
+++ src/services/reset-password.service.ts
@@ -0,0 +1,40 @@
+import { getResetPasswordKey } from '../utils/redis';
+import otpGenerator from 'otp-generator';
+import redis from 'src/configs/Redis';
+import { transporter } from './send-otp.service';
+import * as userService from 'src/services/user.service';
+import { NotFoundException } from 'src/exceptions';
+
+const getOptions = (email: string, host: string, id: string, code: string) => {
+  return {
+    from: 'RVK Chat App',
+    to: email,
+    subject: 'RVK Chat app reset password',
+    text: ' Hello welcome RVK Chat App, your link reset password ',
+    html: `<p> Hello welcome RVK Chat App, your link reset password <b> ${host}/reset-password/${id}?code=${code}</b> </p> `,
+  };
+};
+
+export const sendLink = async (email: string, host: string) => {
+  const user = await userService.getOneOrThrow({ where: { email } });
+
+  const code = otpGenerator.generate(20, { specialChars: false });
+
+  const mainOptions = getOptions(email, host, user.id, code);
+
+  transporter.sendMail(mainOptions);
+
+  redis.set(getResetPasswordKey(user.id), code);
+};
+
+export const resetPassword = async (
+  id: string,
+  code: string,
+  password: string
+) => {
+  const savedCode = await redis.get(getResetPasswordKey(id));
+  if (code !== savedCode) {
+    throw new NotFoundException('user');
+  }
+  return userService.updatePasswordUser({ password }, id);
+};
