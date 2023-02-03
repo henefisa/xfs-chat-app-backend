4b34e23d53013c1a505bb4a9dcdd1b5df3a82516
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-02-03 21:44:13 +0700

    feat: upload file to server

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -3,6 +3,7 @@ import { GetPreSignedUrlDto } from 'src/dto/upload/get-presign-url.dto';
 import { RequestWithBody } from 'src/shares';
 import * as S3Service from 'src/services/s3.service';
 import { StatusCodes } from 'http-status-codes';
+import { NotFoundException } from 'src/exceptions';
 
 export const getPreSignedUrl = async (
   req: RequestWithBody<GetPreSignedUrlDto>,
@@ -13,6 +14,38 @@ export const getPreSignedUrl = async (
     const presigned = S3Service.getPreSignedUrl(req.body.key);
 
     return res.status(StatusCodes.OK).json(presigned);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const uploadFile = async (
+  req: RequestWithBody,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const file = req.file;
+    if (!file) {
+      throw new NotFoundException('file');
+    }
+    return res.status(StatusCodes.OK).json(file);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const uploadMultipleFile = async (
+  req: RequestWithBody,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const files = req.files;
+    if (!files) {
+      throw new NotFoundException('files');
+    }
+    return res.status(StatusCodes.OK).json(files);
   } catch (error) {
     next(error);
   }
