7c43f09751b99aec9ae14a37fd117cfab349cdc4
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-02-03 21:44:13 +0700

    feat: upload file to server

---
+++ src/services/multer.service.ts
@@ -0,0 +1,30 @@
+import { Request } from 'express';
+import multer from 'multer';
+
+export const getDiskStorage = () => {
+  const storage = multer.diskStorage({
+    destination: function (
+      req: Request,
+      file: Express.Multer.File,
+      callback: (error: Error | null, destination: string) => void
+    ) {
+      callback(null, './uploads');
+    },
+
+    filename: function (
+      req: Request,
+      file: Express.Multer.File,
+      callback: (error: Error | null, destination: string) => void
+    ) {
+      const fileName = file.originalname.toLowerCase().split(' ').join('-');
+      callback(null, fileName);
+    },
+  });
+
+  return multer({
+    storage: storage,
+    limits: {
+      fileSize: 8000000,
+    },
+  });
+};
