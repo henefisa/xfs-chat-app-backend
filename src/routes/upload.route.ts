9b19917a895ae0854b173594572661a8ce52b129
khang2038 <84883516+khang2038@users.noreply.github.com>
2023-02-03 23:03:58 +0700

    Feature/upload files to server (#190)
    * feat: get information user when typing
    
    * feat: upload file to server
    
    ---------
    
    Co-authored-by: NgocHung <hoang.ngoc.hungit@gmail.com>

--- src/routes/upload.route.ts
+++ src/routes/upload.route.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { getPreSignedUrl } from 'src/controllers/upload.controller';
+import {
+  getPreSignedUrl,
+  uploadFile,
+  uploadMultipleFile,
+} from 'src/controllers/upload.controller';
 import { GetPreSignedUrlDto } from 'src/dto/upload/get-presign-url.dto';
 import activateMiddleware from 'src/middlewares/activate.middleware';
 import requireAuthMiddleware from 'src/middlewares/require-auth.middleware';
 import validationMiddleware from 'src/middlewares/validation.middleware';
+import * as upload from 'src/services/multer.service';
 const router: Router = Router();
 
 /**
@@ -46,6 +51,70 @@ router.post(
   activateMiddleware,
   validationMiddleware(GetPreSignedUrlDto),
   getPreSignedUrl
+);
+
+/**
+ * @swagger
+ * /api/upload/upload-file:
+ *  post:
+ *     summary: upload file to server
+ *     tags: [Upload]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *              type: object
+ *              properties:
+ *                file:
+ *                  type: string
+ *                  format: binary
+ *     security:
+ *         - bearerAuth: []
+ *     responses:
+ *       200:
+ *          description: upload file successfully
+ */
+
+router.post(
+  '/upload-file',
+  requireAuthMiddleware,
+  activateMiddleware,
+  upload.getDiskStorage().single('file'),
+  uploadFile
+);
+
+/**
+ * @swagger
+ * /api/upload/upload-Multiple-file:
+ *  post:
+ *     summary: upload Multiple file to server
+ *     tags: [Upload]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *              type: object
+ *              properties:
+ *                files:
+ *                  type: array
+ *                  items:
+ *                     type: string
+ *                     format: binary
+ *     security:
+ *         - bearerAuth: []
+ *     responses:
+ *       200:
+ *          description: upload file successfully
+ */
+
+router.post(
+  '/upload-Multiple-file',
+  requireAuthMiddleware,
+  activateMiddleware,
+  upload.getDiskStorage().array('files', 12),
+  uploadMultipleFile
 );
 
 export const uploadRoutes = router;
