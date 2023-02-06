44099dd6909d86d9e82cdb020c6753fb314d6db4
Nguyễn Hồng Huy <58259694+pinkhuy1004@users.noreply.github.com>
2023-02-06 09:51:33 +0700

    feat(server): setup peer server (#193)
    * feat(server): setup peer server
    
    * fix: code smell

--- src/configs/Peer.ts
+++ src/configs/Peer.ts
@@ -1,7 +1,82 @@
-import { PeerServer } from 'peer';
+import { ExpressPeerServer } from 'peer';
+import type { CustomExpress, IConfig } from 'peer';
+import type { Express } from 'express';
+import type { EventEmitter } from 'events';
+import type WebSocketLib from 'ws';
+import type { Server } from 'net';
 
-const peerServer = PeerServer({ port: 8001 }, () => {
-  console.log('Peer server started');
-});
+declare module 'peer' {
+  enum MessageType {
+    OPEN = 'OPEN',
+    LEAVE = 'LEAVE',
+    CANDIDATE = 'CANDIDATE',
+    OFFER = 'OFFER',
+    ANSWER = 'ANSWER',
+    EXPIRE = 'EXPIRE',
+    HEARTBEAT = 'HEARTBEAT',
+    ID_TAKEN = 'ID-TAKEN',
+    ERROR = 'ERROR',
+  }
 
-export default peerServer;
+  interface IMessage {
+    readonly type: MessageType;
+    readonly src: string;
+    readonly dst: string;
+    readonly payload?: any;
+  }
+
+  type MyWebSocket = WebSocketLib & EventEmitter;
+  interface IClient {
+    getId(): string;
+    getToken(): string;
+    getSocket(): MyWebSocket | null;
+    setSocket(socket: MyWebSocket | null): void;
+    getLastPing(): number;
+    setLastPing(lastPing: number): void;
+    send(data: any): void;
+  }
+
+  interface IConfig {
+    readonly port?: number;
+    readonly expire_timeout?: number;
+    readonly alive_timeout?: number;
+    readonly key?: string;
+    readonly path?: string;
+    readonly concurrent_limit?: number;
+    readonly allow_discovery?: boolean;
+    readonly proxied?: boolean | string;
+    readonly cleanup_out_msgs?: number;
+    readonly ssl?: {
+      key: string;
+      cert: string;
+    };
+    readonly generateClientId?: () => string;
+  }
+
+  interface CustomExpress extends Express {
+    on(event: string, callback: (...args: any[]) => void): this;
+    on(event: 'connection', callback: (client: IClient) => void): this;
+    on(event: 'disconnect', callback: (client: IClient) => void): this;
+    on(
+      event: 'message',
+      callback: (client: IClient, message: IMessage) => void
+    ): this;
+    on(event: 'error', callback: (error: Error) => void): this;
+  }
+}
+
+class PeerServer {
+  public static peerServer: CustomExpress;
+
+  constructor(server: Server, options?: IConfig | undefined) {
+    PeerServer.peerServer = ExpressPeerServer(server, options);
+  }
+
+  public handleListenEvent() {
+    PeerServer.peerServer.on('connection', (client) => {
+      console.log('client connected', client);
+    });
+  }
+}
+
+export default PeerServer;
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ dotenv.config();
 
 import Database from './configs/Database';
 import 'reflect-metadata';
-import './configs/Peer';
 const port = process.env.PORT || 8000;
 
 Database.instance.initialize();
 
 import server from './server';
 import { ServerSocket } from './configs/socket';
+import PeerServer from './configs/Peer';
 import http from 'http';
 import https from 'https';
 
@@ -36,6 +36,16 @@ if (process.env.NODE_ENV === 'production') {
 
   const httpsServer = https.createServer(credentials, server);
   new ServerSocket(httpsServer).start();
+  const peerServer = new PeerServer(httpsServer, {
+    ssl: {
+      key: privateKey,
+      cert: certificate,
+    },
+  });
+
+  server.use('/peerjs', PeerServer.peerServer);
+
+  peerServer.handleListenEvent();
 
   httpsServer.listen(port, () => {
     console.log(`HTTPS server is listen on port ${port}`);
@@ -43,6 +53,11 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   const httpServer = http.createServer(server);
   new ServerSocket(httpServer).start();
+  const peerServer = new PeerServer(httpServer, undefined);
+
+  server.use('/peerjs', PeerServer.peerServer);
+
+  peerServer.handleListenEvent();
 
   httpServer.listen(port, () => {
     console.log(`HTTP server is listen on port ${port}`);
